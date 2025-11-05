686a4ac2274092ee1769710688dc23b72336a2a6
vitor <vitor.oliveira.rolla@gmail.com>
2025-11-05 09:17:23 -0300

    Estrutura de Árvore B+

---
+++ Aed_3/Códigos/Estruturas/Árvore B+/RegistroArvoreBMais.java
@@ -0,0 +1,12 @@
+package service.Genérico.Árvore;
+
+import java.io.IOException;
+
+public interface RegistroArvoreBMais<T> {
+
+  public short size(); 
+  public byte[] toByteArray() throws IOException; 
+  public void fromByteArray(byte[] ba) throws IOException; 
+  public int compareTo(T obj); 
+  public T clone(); 
+}
