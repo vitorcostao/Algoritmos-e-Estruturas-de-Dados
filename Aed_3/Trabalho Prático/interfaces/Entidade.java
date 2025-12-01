1723337a36cc2eb2a009f93c334075bb5508924f
vitor <vitor.oliveira.rolla@gmail.com>
2025-12-01 14:30:51 -0300

    Trabalho prático

--- Aed_3/Trabalho Prático/interfaces/Entidade.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface Entidade {
-    
-    public int getId();
-    public void setId(int id);
-    public byte[] toByteArray() throws Exception;
-    public void fromByteArray(byte[] vb) throws Exception;
-}
